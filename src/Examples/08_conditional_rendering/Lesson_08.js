8dab87a6bbfff61ab32fa36234683a50c6127d2a
Andrei <stigmaby@gmail.com>
2020-09-29 22:37:47 +0300

    #7 Условный рендеринг (Conditional Rendering)

---
+++ src/Examples/08_conditional_rendering/Lesson_08.js
@@ -0,0 +1,49 @@
+import React, {Component} from "react";
+
+const ValidationMsg = ({val}) => {
+    if (val < 10) {
+        return <h2>Less then 10</h2>
+    } else {
+        return <h2>More then 10</h2>
+    }
+}
+
+const Tab1 = () => {
+    return <h1>Text of tab1</h1>
+}
+const Tab2 = () => {
+    return <h1>Text of tab2</h1>
+}
+const Tab3 = () => {
+    return <h1>Text of tab3</h1>
+}
+
+export class Lesson_08 extends Component {
+    state = {
+        activeTab: 1
+    }
+    handleClick = (e) => {
+        this.setState({
+            activeTab: +e.target.getAttribute('data-name')
+        })
+    }
+
+    render() {
+        const {activeTab} = this.state
+        console.log(this.state)
+        return (
+            <div>
+                <button data-name={1} onClick={this.handleClick}>Tab1</button>
+                <button data-name={2} onClick={this.handleClick}>Tab2</button>
+                <button data-name={3} onClick={this.handleClick}>Tab3</button>
+                {/*{activeTab === 1 && <Tab1 />}*/}
+                {/*{activeTab === 2 && <Tab2 />}*/}
+                {/*{activeTab === 3 && <Tab3 />}*/}
+                {activeTab === 1 ? <Tab1 /> : activeTab === 2 ? <Tab2 /> : <Tab3 />}
+                <div>
+                    {`Active tab is: ${activeTab === 1 ? 'First' : activeTab === 2 ? 'Second' : 'Third'}`}
+                </div>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ import {Lesson_02} from "./Examples/02_components/Lesson_02";
 import {Lesson_03} from "./Examples/03_state/Lesson_03";
 import {Lesson_04} from "./Examples/04_props/Lesson_04";
 import {Lesson_06} from "./Examples/06_events/Lesson_06";
+import {Lesson_08} from "./Examples/08_conditional_rendering/Lesson_08";
 
 React.cloneElement('div', null, 'Just div')
 React.cloneElement('input', {className: 'justInput'})
 
 ReactDOM.render(
   <React.StrictMode>
-    <Lesson_06 />
+    <Lesson_08 />
   </React.StrictMode>,
   document.getElementById('root')
 );
