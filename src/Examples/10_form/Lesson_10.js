db52752c71bd556c6b646814630c4d80d84b9718
Andrei <stigmaby@gmail.com>
2020-10-01 13:07:19 +0300

    #10 is done, forms + select

--- src/Examples/10_form/Lesson_10.js
+++ src/Examples/10_form/Lesson_10.js
@@ -4,9 +4,11 @@ export class Lesson_10 extends Component {
     state = {
         inputText: '',
         textAreaText: '',
+        selectText: '',
         showData: {
             name: '',
-            text: ''
+            text: '',
+            position: ''
         }
     }
     // inputChangeHandler = (e) => {
@@ -26,34 +28,46 @@ export class Lesson_10 extends Component {
             textAreaText: value
         })
     }
+    selectTextChangeHandler = ( {target: {value}} ) => {
+        this.setState({
+            selectText: value
+        })
+    }
     onButtonClick = (e) => {
         e.preventDefault()
-        const {inputText, textAreaText} = this.state
+        const {inputText, textAreaText, selectText} = this.state
         this.setState({
             inputText: '',
             textAreaText: '',
+            selectText: '',
             showData: {
                 name: inputText,
-                text: textAreaText
+                text: textAreaText,
+                position: selectText
             }
         })
         console.log(this.state)
     }
 
     render() {
-        const {inputText, textAreaText, showData} = this.state
-        const {name, text} = showData
+        const {inputText, textAreaText, showData, selectText} = this.state
+        const {name, text, position} = showData
 
         return (
             <Fragment>
                 <form>
                     <input type={'text'} value={inputText} onChange={this.inputChangeHandler}/><br/>
                     <textarea value={textAreaText} onChange={this.textAreaChangeHandler}/><br/>
-                    <button onClick={this.onButtonClick}>Click</button>
+                    <select value={selectText} onChange={this.selectTextChangeHandler}>
+                        <option value={'Frontend-Dev'}>Frontend-Dev</option>
+                        <option value={'Backend-Dev'}>Backend-Dev</option>
+                    </select>
                 </form>
+                <button onClick={this.onButtonClick}>Click</button>
 
                 <h2>{name}</h2>
                 <h2>{text}</h2>
+                <h2>{position}</h2>
                 {/*<span>{this.state.showData.name}</span>*/}
                 {/*<span>{this.state.showData.text}</span>*/}
             </Fragment>
